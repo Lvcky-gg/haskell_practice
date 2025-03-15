ef40b9296ed9f4243e95ac0fc1d90cb2eb4bee7e
Your Name <matadorcrab@gmail.com>
2025-03-15 10:29:04 -0400

    recurse

--- l4/higher_func.hs
+++ l4/higher_func.hs
@@ -1,4 +1,18 @@
 -- map
+--foldl
+--foldr
+--filter
+--zipWith
+--takeWhile
+--dropWhile
+--iterate
+--repeat
+--replicate
+--cycle
+--scanl
+--scanr
+--sort
+import Data.List (sort)
 inc :: Int -> Int
 isEven :: Int -> Bool
 isEven x = x `mod` 2 == 0
@@ -7,3 +21,5 @@ inc x = x + 1
 main = do
     print(map inc [1, 2, 3, 4, 5])
     print(filter isEven [1, 2, 3, 4, 5])
+    print(foldl (*) 2 [1, 2, 3, 4, 5])
+    print(sort [5, 4, 3, 2, 1,7,4,2,90,43,535,123,446,223])
