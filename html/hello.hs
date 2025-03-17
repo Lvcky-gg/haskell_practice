b65540c3a1a0e7629af15ad0f1eb29c735d41934
Your Name <matadorcrab@gmail.com>
2025-03-17 18:42:42 -0400

    chore

--- html/hello.hs
+++ html/hello.hs
@@ -1,11 +1,17 @@
 html_ :: String -> String
 body_ :: String -> String
-wrapHtml :: String -> String
+head_ :: String -> String
+title_ :: String -> String
 myHtml :: String
+makeHtml :: String -> String -> String
 
 html_ html= "<html>"<>html<>"</html>"
 body_ body= "<body>"<>body<>"</body>"
-wrapHtml content = html_ (body_ content)
-myHtml =wrapHtml "Hello World"
+
+head_ head= "<head>"<>head<>"</head>"
+title_ title= "<title>"<>title<>"</title>"
+myHtml = makeHtml "Hello title" "Hello, world!"
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+
 
 main = putStrLn myHtml
