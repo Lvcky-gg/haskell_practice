a607e62ed294903f0e481f956c87e4a1f5fd5ebc
Your Name <matadorcrab@gmail.com>
2025-03-17 18:36:44 -0400

    chore

--- html/hello.hs
+++ html/hello.hs
@@ -1,2 +1,11 @@
+html_ :: String -> String
+body_ :: String -> String
+wrapHtml :: String -> String
+myHtml :: String
+
+html_ html= "<html>"<>html<>"</html>"
+body_ body= "<body>"<>body<>"</body>"
+wrapHtml content = html_ (body_ content)
+myHtml =wrapHtml "Hello World"
+
 main = putStrLn myHtml
-myHtml ="<html><body>Hello World</body></html>"
