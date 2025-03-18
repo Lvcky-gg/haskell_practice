d140d53d6977526f747fb33e36260555fc70e4d3
Your Name <matadorcrab@gmail.com>
2025-03-17 20:16:23 -0400

    chore

--- html/hello.hs
+++ html/hello.hs
@@ -1,17 +1,34 @@
-html_ :: String -> String
-body_ :: String -> String
-head_ :: String -> String
-title_ :: String -> String
-myHtml :: String
+main :: IO ()
+main = putStrLn myhtml
+
+myhtml :: String
+myhtml =
+  makeHtml
+    "Hello title"
+    (h1_ "Hello, world!" <> p_ "Let's learn about Haskell!")
+
+
 makeHtml :: String -> String -> String
-
-html_ html= "<html>"<>html<>"</html>"
-body_ body= "<body>"<>body<>"</body>"
-
-head_ head= "<head>"<>head<>"</head>"
-title_ title= "<title>"<>title<>"</title>"
-myHtml = makeHtml "Hello title" "Hello, world!"
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
 
+html_ :: String -> String
+html_ = el "html"
 
-main = putStrLn myHtml
+body_ :: String -> String
+body_ = el "body"
+
+head_ :: String -> String
+head_ = el "head"
+
+title_ :: String -> String
+title_ = el "title"
+
+p_ :: String -> String
+p_ = el "p"
+
+h1_ :: String -> String
+h1_ = el "h1"
+
+el :: String -> String -> String
+el tag content =
+  "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
