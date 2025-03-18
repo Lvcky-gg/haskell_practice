6855f666e8120d118b27aa183f7346279b7aeee7
Your Name <matadorcrab@gmail.com>
2025-03-18 14:32:25 -0400

    chore

--- modules/m3.hs
+++ modules/m3.hs
@@ -1,8 +1,9 @@
 import Data.Map (Map)
-import qualified Data.Map as Map
-myMap :: Int -> Map Int Int
-myMap n = Map.fromList(map makePair [1..n])
-    where makePair x = (x, [x])
+import Data.Map qualified as Map
 
-main = print(myMap 5)
-    
+myMap :: Int -> Map Int Int
+myMap n = Map.fromList (map makePair [1 .. n])
+  where
+    makePair x = (x, x)
+
+main = print (myMap 5)
